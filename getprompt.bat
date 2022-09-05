19e6baf42753cce7ef2a43e9c5f848bd6d388d5c
Quinn <qdirks@gmail.com>
2022-09-05 10:54:15 -0500

    Update getprompt.bat

--- getprompt.bat
+++ getprompt.bat
@@ -1,4 +1,4 @@
 @echo off
-@rem Get prompt at batch file location. Useful for quickly getting an administrator prompt at this directory location by running this script as administrator.
+@rem Get prompt at batch file location. Useful for quickly getting an administrator prompt at this directory location by running as administrator.
 cd /d %~dp0
 cmd /k

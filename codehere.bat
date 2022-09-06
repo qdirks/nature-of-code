b720b77c6803e0b07638ff9f154457c497894fa9
Quinn Dirks <qdirks@gmail.com>
2022-09-06 17:10:34 -0500

    update comment

--- codehere.bat
+++ codehere.bat
@@ -4,7 +4,7 @@
 @REM Change to the directory of this batch script
 cd /d %~dp0
 
-@REM start code in a manner that allows the cmd window to close afterwards
+@REM start code in a manner that allows the window to close afterwards
 echo | code . | exit
 
 @REM You can run more commands here if needed...

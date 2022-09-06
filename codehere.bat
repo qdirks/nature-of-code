b1524e7eefcd8c8b2640d8f5d17d0735384ef89b
Quinn Dirks <qdirks@gmail.com>
2022-09-06 17:08:27 -0500

    comment update'

--- codehere.bat
+++ codehere.bat
@@ -1,7 +1,11 @@
 @echo off
 
-@REM Change to this directory
+@REM When running as administrator, the prompt starts at C:\WINDOWS\system32
+@REM Change to the directory of this batch script
 cd /d %~dp0
 
-@REM Start code and have the cmd window close afterwards
+@REM start code in a manner that allows the cmd window to close afterwards
 echo | code . | exit
+
+@REM You can run more commands here if needed...
+@REM No more commands? The window closes very quickly.

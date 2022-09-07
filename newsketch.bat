eda32d151b50cda510557f4b8ac7c340e362d2c3
Quinn Dirks <qdirks@gmail.com>
2022-09-07 01:40:35 -0500

    update sketch creation process

--- newsketch.bat
+++ newsketch.bat
@@ -1,12 +1,22 @@
 @echo off
-@rem Create a new sketch from template. Must be run as administrator in order to copy symbolic links.
 
+@REM When running as administrator, a new cmd instance starts in a different directory
+@REM Change to the same directory as this script
 cd /d %~dp0
-setlocal
-set fname=""
-set /p fname="New Sketch Name: "
-if %fname%=="" (
+
+@REM Check for a command line argument passed to the batch script
+if -%1-==-- (
     echo A sketch project name was not specified. Closing the creation process...
+    exit /b
 ) else (
-    xcopy /s /b /i template %fname%
+    xcopy /s /b /i template %1
 )
+
+@REM Create the index.html file and insert the title of the project
+cd %1
+type ..\shared\index1.html > index.html
+echo.>> index.html
+echo     ^<title^>%1^</title^>>> index.html
+type ..\shared\index2.html >> index.html
+
+gulp

317738b69e53a1b9bfb47fb9a9d97ad3a5e5d626
Quinn Dirks <qdirks@gmail.com>
2022-09-10 23:00:37 -0500

    added check for admin privilege

--- newsketch.bat
+++ newsketch.bat
@@ -1,22 +1,33 @@
 @echo off
 
+@REM Detect if script has the required permissions
+net session >nul 2>&1
+if not %errorLevel% == 0 (
+    echo Error: Retry running this script as administrator.
+    exit /b %errorlevel%
+)
+
 @REM When running as administrator, a new cmd instance starts in a different directory
 @REM Change to the same directory as this script
 cd /d %~dp0
 
 @REM Check for a command line argument passed to the batch script
 if -%1-==-- (
-    echo A sketch project name was not specified. Closing the creation process...
-    exit /b
+    set /p __new_directory__="Enter sketch name: "
 ) else (
-    xcopy /s /b /i template %1
+    set __new_directory__=%1
 )
+if -%__new_directory__%-==-- (
+    echo Error: A sketch name was not specified.
+    exit /b 1
+)
+xcopy /s /b /i template %__new_directory__%
+cd %__new_directory__%
 
 @REM Create the index.html file and insert the title of the project
-cd %1
 type ..\shared\index1.html > index.html
 echo.>> index.html
-echo     ^<title^>%1^</title^>>> index.html
+echo     ^<title^>%__new_directory__%^</title^>>> index.html
 type ..\shared\index2.html >> index.html
 
 gulp
